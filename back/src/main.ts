4ce756b303b4347cdaa00efb485f894418743126
Muhammadrizo <mtursunboev13@gmail.com>
2023-06-21 15:14:06 +0500

    init

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(port);
-  console.log(`Server running on port http://localhost:${port}/`);
+  console.log(`Server running on http://1627061-ci09322.twc1.net:${port}/`);
 }
 bootstrap();
